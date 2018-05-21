9c4bee90698cc1c3c6973c54c68af092e660054a
paradisepilot <paradisepilot@gmail.com>
2018-05-21 19:34:14 -0400

    updated things ...

--- vm-startups/vm-startup-01/vm-startup-01.sh
+++ vm-startups/vm-startup-01/vm-startup-01.sh
@@ -8,14 +8,15 @@ cd    ./tmp
 
 minicondaInstaller=Miniconda3-latest-Linux-x86_64.sh
 curl -O https://repo.continuum.io/miniconda/${minicondaInstaller}
-#bash ${minicondaInstaller}
-#conda update conda
+bash ${minicondaInstaller} -b -p $HOME/miniconda
+export PATH="$HOME/miniconda/bin:$PATH"
+source $HOME/miniconda/bin/activate
+
+conda update  --yes conda
+conda install --yes pandas scikit-learn
+#conda install --yes keras
 
 #anacondaInstaller=Anaconda3-5.1.0-Linux-x86_64.sh
 #curl -O https://repo.anaconda.com/archive/${anacondaInstaller}
-#bash ${anacondaInstaller}
-
-#source ~/.bashrc
-#conda install pandas scikit-learn
-#conda install keras
+#bash ${anacondaInstaller} -b -p $HOME/anaconda
 
