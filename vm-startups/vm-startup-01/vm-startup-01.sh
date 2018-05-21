81115830c320d1f84d3803e9defafb48e880516f
paradisepilot <paradisepilot@gmail.com>
2018-05-21 17:23:42 -0400

    updated things ...

--- vm-startups/vm-startup-01/vm-startup-01.sh
+++ vm-startups/vm-startup-01/vm-startup-01.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-#sudo apt-get update
-#sudo apt-get install r-base r-base-dev
+sudo apt-get update
+sudo apt-get install r-base r-base-dev
 
 mkdir ./tmp
 cd    ./tmp
 
 minicondaInstaller=Miniconda3-latest-Linux-x86_64.sh
 curl -O https://repo.continuum.io/miniconda/${minicondaInstaller}
-#bash ${minicondaInstaller}
+bash ${minicondaInstaller}
 
 #anacondaInstaller=Anaconda3-5.1.0-Linux-x86_64.sh
 #curl -O https://repo.anaconda.com/archive/${anacondaInstaller}
 #bash ${anacondaInstaller}
 
-# source ~/.bashrc
+source ~/.bashrc
 
-#conda install scikit-learn
-#conda install keras
+conda install scikit-learn
+conda install keras
 
