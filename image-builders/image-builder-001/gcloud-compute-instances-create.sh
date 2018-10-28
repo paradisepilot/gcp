1515e7ca62cc63f925bc985ce97cc1cc2f1f6789
paradisepilot <paradisepilot@gmail.com>
2018-10-28 13:55:24 -0400

    updated things ...

---
+++ image-builders/image-builder-001/gcloud-compute-instances-create.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+gcloud compute instances create voyager \
+    --zone          northamerica-northeast1-a \
+    --machine-type  n1-standard-1 \
+    --image         ubuntu-minimal-1810-cosmic-v20181018 \
+    --image-project ubuntu-os-cloud
+
