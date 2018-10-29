a761debade1e5f1decb715d68c60cdd9690dbfb7
paradisepilot <paradisepilot@gmail.com>
2018-10-29 00:40:31 -0400

    updated things ...

--- image-builders/image-builder-001/image-builder-001.sh
+++ image-builders/image-builder-001/image-builder-001.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-sudo apt-get update
-sudo apt-get upgrade
+sudo apt-get    update
+sudo apt-get -y upgrade
 
 ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
 ### install Anaconda dependencies
--- image-builders/image-builder-001/myStartupScript.sh
+++ image-builders/image-builder-001/myStartupScript.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 touch /home/`whoami`/STARTUP-BEGINS.txt
+sudo apt-get    update
+sudo apt-get -y upgrade
+sudo apt-get -y install git
+git clone https://github.com/paradisepilot/gcp
 touch /home/`whoami`/STARTUP-COMPLETE.txt
