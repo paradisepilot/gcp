2715dc719f17e3708e6886154dca315db9502394
paradisepilot <paradisepilot@gmail.com>
2018-10-28 23:51:46 -0400

    updated things ...

--- image-builders/image-builder-001/myStartupScript.sh
+++ image-builders/image-builder-001/myStartupScript.sh
@@ -3,7 +3,11 @@
 sudo apt-get -y update
 sudo apt-get -y upgrade
 
-sudo apt-get -y install git
+myUsername=`whoami`
+myHOME=/home/${myUsername}
 
+sudo apt-get -y install git
 sudo git clone https://github.com/paradisepilot/gcp
+
+touch ${myHOME}/STARTUP-COMPLETE.txt
 
