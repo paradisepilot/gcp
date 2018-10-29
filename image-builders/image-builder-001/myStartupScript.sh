377f9768f60c8fdfb855cfcec4d593dde66fe349
paradisepilot <paradisepilot@gmail.com>
2018-10-29 00:30:51 -0400

    updated things ...

--- image-builders/image-builder-001/myStartupScript.sh
+++ image-builders/image-builder-001/myStartupScript.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 touch /home/`whoami`/STARTUP-BEGINS.txt
-sudo apt-get -y update
-sudo apt-get -y upgrade
-sudo apt-get -y install git
-sudo git clone https://github.com/paradisepilot/gcp
 touch /home/`whoami`/STARTUP-COMPLETE.txt
