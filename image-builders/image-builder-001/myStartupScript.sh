02db67b3202178c74ae8f9cfc4853db935b1db02
paradisepilot <paradisepilot@gmail.com>
2018-10-29 00:05:42 -0400

    updated things ...

--- image-builders/image-builder-001/myStartupScript.sh
+++ image-builders/image-builder-001/myStartupScript.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-sudo apt-get -y update
-sudo apt-get -y upgrade
-
 myUsername=`whoami`
 myHOME=/home/${myUsername}
+touch ${myHOME}/STARTUP-BEGINS.txt
+
+
+sudo apt-get -y update
+sudo apt-get -y upgrade
 
 sudo apt-get -y install git
 sudo git clone https://github.com/paradisepilot/gcp
