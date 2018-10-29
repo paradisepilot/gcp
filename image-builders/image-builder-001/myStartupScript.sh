83ec4adc092592856899a1210c38250a5f002bbd
paradisepilot <paradisepilot@gmail.com>
2018-10-29 00:20:07 -0400

    updated things ...

--- image-builders/image-builder-001/myStartupScript.sh
+++ image-builders/image-builder-001/myStartupScript.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
-
-myUsername=`whoami`
-myHOME=/home/${myUsername}
-touch ${myHOME}/STARTUP-BEGINS.txt
-
+touch /home/`whoami`/STARTUP-BEGINS.txt
 sudo apt-get -y update
 sudo apt-get -y upgrade
-
 sudo apt-get -y install git
 sudo git clone https://github.com/paradisepilot/gcp
-
-touch ${myHOME}/STARTUP-COMPLETE.txt
-
+touch /home/`whoami`/STARTUP-COMPLETE.txt
