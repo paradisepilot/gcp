74b162b8f4cde221a0a98327e7e91c7e8dd9126d
paradisepilot <paradisepilot@gmail.com>
2018-11-13 23:28:28 -0500

    updated things ...

--- image-builders/image-builder-001/gcloud-compute-instances-create.sh
+++ image-builders/image-builder-001/gcloud-compute-instances-create.sh
@@ -3,7 +3,8 @@
 gcloud compute instances create voyager \
     --zone               northamerica-northeast1-c \
     --machine-type       n1-standard-1 \
-    --image              ubuntu-1804-bionic-v20181024 \
+#   --image              ubuntu-1804-bionic-v20181024 \
+    --image              ubuntu-1810-cosmic-v20181018 \
     --image-project      ubuntu-os-cloud \
     --metadata-from-file startup-script=myStartupScript.sh
 
