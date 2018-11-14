cf7ee3edd83da43c01003abcc4df0f4a5a16dc73
paradisepilot <paradisepilot@gmail.com>
2018-11-14 00:04:58 -0500

    updated things ...

--- image-builders/image-builder-001/gcloud-compute-instances-create.sh
+++ image-builders/image-builder-001/gcloud-compute-instances-create.sh
@@ -3,7 +3,6 @@
 gcloud compute instances create voyager \
     --zone               northamerica-northeast1-c \
     --machine-type       n1-standard-1 \
-#   --image              ubuntu-1804-bionic-v20181024 \
     --image              ubuntu-1810-cosmic-v20181018 \
     --image-project      ubuntu-os-cloud \
     --boot-disk-size     100GB \
