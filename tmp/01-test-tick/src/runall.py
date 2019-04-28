645a8c09d235672fc9cd81031655274d30ae8375
paradisepilot <paradisepilot@gmail.com>
2019-04-28 12:21:06 -0400

    added to runall.py: printing system time at beginning and end of execution + printing IPython.sys_info() and sinfo.sinfo() right before quitting

--- tmp/01-test-tick/src/runall.py
+++ tmp/01-test-tick/src/runall.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import os, stat, sys
+import os, stat, sys, datetime
 
 thisScript = sys.argv[0]
 srcDIR     = os.path.normpath(sys.argv[1])
@@ -12,8 +12,12 @@ sys.path.append(srcDIR)
 # change directory to outDIR
 os.chdir(outDIR)
 
+print( "\n########################################" )
+print( "\n### datetime.datetime.now(): " + str(datetime.datetime.now()) )
+
 #################################################
 #################################################
+import sinfo, IPython
 
 ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
 from testHawkesEM import test_HawkesEM
@@ -21,5 +25,14 @@ test_HawkesEM()
 
 #################################################
 #################################################
+print("\n\n########################################" )
+
+print("\n### IPython.sys_info():")
+print(       IPython.sys_info()  )
+
+print("\n### sinfo.info():")
+print(       sinfo.sinfo(print_std_lib = True, print_implicit = True)  )
+
+print("\n### datetime.datetime.now(): " + str(datetime.datetime.now()) + "\n" )
 sys.exit(0)
 
