8164a313cfd10058d918110058fca9fac9fa713c
paradisepilot <paradisepilot@gmail.com>
2019-04-28 18:55:10 +0000

    added: output.2019-04-28.01/

---
+++ tmp/01-test-tick/output.2019-04-28.01/src/runall.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+import os, stat, sys, datetime
+
+thisScript = sys.argv[0]
+srcDIR     = os.path.normpath(sys.argv[1])
+outDIR     = os.path.normpath(sys.argv[2])
+
+# append module path with srcDIR
+sys.path.append(srcDIR)
+
+# change directory to outDIR
+os.chdir(outDIR)
+
+time_string = str(datetime.datetime.now())
+tz_string   = datetime.datetime.now(datetime.timezone.utc).astimezone().tzname()
+print( "\n### system time: " + time_string + " " + tz_string )
+print( "\n########################################" )
+
+#################################################
+#################################################
+import sinfo, IPython
+
+### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+from testHawkesEM import test_HawkesEM
+test_HawkesEM()
+
+#################################################
+#################################################
+print("\n\n########################################" )
+
+print("\n### IPython.sys_info():")
+print(       IPython.sys_info()  )
+
+print("\n### sinfo.info():")
+print(       sinfo.sinfo(print_std_lib = True, print_implicit = True)  )
+
+print("\n########################################" )
+time_string = str(datetime.datetime.now())
+tz_string   = datetime.datetime.now(datetime.timezone.utc).astimezone().tzname()
+print( "\n### system time: " + time_string + " " + tz_string + "\n" )
+sys.exit(0)
+
