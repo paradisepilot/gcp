6ba5a07685d80701b869590f60ca422473fcc341
Kenneth Chu <kennethchu@Kenneths-MacBook-Pro.local>
2021-11-14 15:49:47 -0500

    updated: gcloud-composer-environments-delete.sh

--- gcloud-composer/project-gauss100/gcloud-composer-environments-delete.sh
+++ gcloud-composer/project-gauss100/gcloud-composer-environments-delete.sh
@@ -5,7 +5,8 @@ source ./global-parameters.txt
 ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
 # delete environment
 gcloud composer environments delete ${ENVIRONMENT_NAME} \
-    --location ${LOCATION}
+    --location ${LOCATION} \
+    --quiet
 
 ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
 # delete environment's disks
@@ -18,10 +19,10 @@ DISK_NAMES=(`gcloud compute disks list | tail -n +2 | awk '{print $1}'`)
 for diskname in "${DISK_NAMES[@]}"
 do
     echo deleting disk: ${diskname}
-    gcloud compute disks delete ${diskname} --zone=${ZONE}
+    gcloud compute disks delete ${diskname} --zone=${ZONE} --quiet
 done
 
 ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
 # remove environment's bucket
-# gsutil rb ${BUCKET_NAME}
+# gsutil rb ${BUCKET_NAME} --quiet
 
