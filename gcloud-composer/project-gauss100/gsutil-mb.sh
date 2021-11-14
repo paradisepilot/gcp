dd0d87f0fa9e1ada4970ad550792f7f1468de20e
Kenneth Chu <kennethchu@Kenneths-MacBook-Pro.local>
2021-11-13 23:55:32 -0500

    added: gsutil-mb.sh

---
+++ gcloud-composer/project-gauss100/gsutil-mb.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+source ./global-parameters.txt
+
+# set the active project
+echo
+echo setting active project to: ${PROJECT_ID}
+echo
+gcloud config set project ${PROJECT_ID}
+
+### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+# create bucket: https://cloud.google.com/storage/docs/quickstart-gsutil
+gsutil mb -l ${LOCATION} -c standard ${BUCKET_NAME}
+
