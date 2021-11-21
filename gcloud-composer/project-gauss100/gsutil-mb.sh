a0115c3385be049e0f0069cc9632f4b9219d1bad
Kenneth Chu <kennethchu@Kenneths-MacBook-Pro.local>
2021-11-21 15:22:16 -0500

    updated: gsutil-mb.sh

--- gcloud-composer/project-gauss100/gsutil-mb.sh
+++ gcloud-composer/project-gauss100/gsutil-mb.sh
@@ -10,10 +10,19 @@ echo
 gcloud config set project ${PROJECT_ID}
 
 ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
-# create bucket: https://cloud.google.com/storage/docs/quickstart-gsutil
-gsutil mb -l ${LOCATION} -c standard ${BUCKET_NAME}
+### create bucket: https://cloud.google.com/storage/docs/quickstart-gsutil
+# gsutil mb -l ${LOCATION} -c standard ${BUCKET_NAME}
 
 ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
-# upload data to bucket
-gsutil -m cp -r input-files ${BUCKET_NAME}/input
+### upload data to bucket
+# gsutil -m cp -r input-files ${BUCKET_NAME}/input
+
+### ########################################### ###
+### The following gsutil commands work on a Composer worker node
+
+### copy (input) data from an existing Cloud Storage bucket
+# mkdir ~/gcs/data/input ; gsutil cp gs://gauss100-2021-11-13-a-bucket/input/input-file-01.csv ~/gcs/data/input
+
+### copy the worker node folder 'output-00' to a Cloud Storage bucket with a specified folder name ('output-00-a')
+# gsutil cp -r ~/gcs/data/output-00 gs://gauss100-2021-11-13-a-bucket/output-00-a
 
