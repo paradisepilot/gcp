0cef8d8fefb78b2b50001f9eef4ec82aac128caf
Kenneth Chu <kennethchu@Kenneths-MacBook-Pro.local>
2021-11-26 08:58:53 -0500

    updated: gcloud-container-node-pools-delete.sh

--- gcloud-composer/project-gauss100/gcloud-container-node-pools-delete.sh
+++ gcloud-composer/project-gauss100/gcloud-container-node-pools-delete.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+gcloud config set container/cluster ${COMPOSER_GKE_NAME}
 gcloud container node-pools delete `gcloud container node-pools list --zone=us-central1-a | egrep -v '(NAME|default-pool)' | awk '{print $1}'` --zone ${COMPOSER_GKE_ZONE} --cluster ${COMPOSER_GKE_NAME} --quiet
 
