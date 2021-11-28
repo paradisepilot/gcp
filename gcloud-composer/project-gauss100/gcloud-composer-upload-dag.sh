e56937d5d33f3dd167461b2a862ded0023e9b16c
Kenneth Chu <kennethchu@Kenneths-MacBook-Pro.local>
2021-11-28 13:10:21 -0500

    updated: gcloud-composer-upload-dag.sh

--- gcloud-composer/project-gauss100/gcloud-composer-upload-dag.sh
+++ gcloud-composer/project-gauss100/gcloud-composer-upload-dag.sh
@@ -4,13 +4,12 @@
 source ./global-parameters.txt
 
 # set the active project
-echo
-echo setting active project to: ${PROJECT_ID}
-echo
+echo; echo setting active project to: ${PROJECT_ID}
 gcloud config set project ${PROJECT_ID}
 
 ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
 ### upload a DAG to Composer environment
+echo; echo Executing: gcloud composer environments storage dags import
 gcloud composer environments storage dags import \
     --environment ${ENVIRONMENT_NAME} \
     --location ${LOCATION} \
